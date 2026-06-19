b5d48f8de29931cba38c40972701d5df2d0ec703
Tarunna Sen <tarunnasen@gmail.com>
2026-06-20 03:02:56 +0600

    User Hiring request and rendering

--- index.js
+++ index.js
@@ -29,7 +29,8 @@ const client = new MongoClient(URI, {
 async function run() {
   try {
     const db = client.db("legal-ease");
-    const lawyersCollection = db.collection("lawyersCollection")
+    const lawyersCollection = db.collection("lawyersCollection");
+    const hiringCollection = db.collection("hiringCollection");
 
     app.get("/", (req, res) => {
         res.send("App is running");
@@ -47,6 +48,12 @@ async function run() {
     app.get("/lawyers/top", async (req, res) => {
       const lawyers = await lawyersCollection.find({}).sort({ gotHired: -1 }).limit(3).toArray();
       res.send(lawyers)
+    })
+
+    app.get("/lawyers/find/:id", async (req, res) => {
+      const { id } = req.params;
+      const lawyer = await lawyersCollection.findOne({ user: id});
+      res.send(lawyer);
     })
 
     // All Lawyers
@@ -67,6 +74,25 @@ async function run() {
       const { id } = req.params;
       const lawyer = await lawyersCollection.findOne({ user: id });
       res.json(lawyer);
+    });
+
+    app.get("/user/hiring-history/:id", async(req, res) => {
+      const { id } = req.params;
+      const hiring = await hiringCollection.find({ userId: id }).toArray();
+      res.json(hiring);
+    })
+
+    app.get("/lawyer/hiring-history/:id", async(req, res) => {
+      const { id } = req.params;
+      const hiring = await hiringCollection.find({ lawyerId: id }).toArray();
+      res.json(hiring);
+    })
+
+    app.post("/hiring", async (req, res) => {
+      const hireData = req.body;
+      const result = await hiringCollection.insertOne(hireData);
+
+      res.json(result);
     });
 
     // Updated Lawyers Profile
