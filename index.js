62b1a7ef23586065f11454e81870d4bb35a468aa
Tarunna Sen <tarunnasen@gmail.com>
2026-06-19 16:08:04 +0600

    Basic Server Setup

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
+
+const PORT = process.env.PORT;
+const URI = process.env.MONGO_URI;
+
+const client = new MongoClient(URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    const db = client.db("legal-ease");
+    // collection = db.collection("ideas");
+
+    app.get("/", (req, res) => {
+        res.send("App is running");
+    })
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } finally {}
+}
+
+run().catch(console.dir);
