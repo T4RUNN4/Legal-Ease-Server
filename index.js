96cba57ca674f98a3f62e45a047ba3f80f2eb817
Tarunna Sen <tarunnasen@gmail.com>
2026-06-22 20:54:53 +0600

    Temporary testing change

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: process.env.CLIENT_URL || "*",
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   }),
